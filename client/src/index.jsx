250b6617e9626560e97c8f84f091c920e7eeda58
Beth Johnson <bethjohnson3@gmail.com>
2018-04-01 14:30:32 -0700

    feat(client): initialize client dev environment
    initialize webpack, eslint, commitizen, jest, react

---
+++ client/src/index.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+render(<p>hi from react { 1 + 1 }</p>, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+const SRC_DIR = path.resolve(__dirname, 'client/src');
+const DIST_DIR = path.resolve(__dirname, 'client/dist');
+
+module.exports = {
+  entry: `${SRC_DIR}/index.jsx`,
+  output: {
+    path: DIST_DIR,
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  mode: 'development',
+};
