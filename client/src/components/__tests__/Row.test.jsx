88e55df20cf5175562a3f0d432d29bc446ee4bc3
Beth Johnson <bethjohnson3@gmail.com>
2018-04-01 18:51:03 -0700

    feat(Row.jsx): add basic rendering of Row component

---
+++ client/src/components/__tests__/Row.test.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Row from '../Row';
+import BoardConstructor from '../../lib/Board';
+
+describe('Row Component', () => {
+  it('renders correctly', () => {
+    const board = new BoardConstructor(16);
+    const domTree = renderer.create(<Row squares={board.squares[0]} />).toJSON();
+    expect(domTree).toMatchSnapshot();
+  });
+});
