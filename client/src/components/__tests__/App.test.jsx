94ab942e436fabc127c94221b4effa615a9e15db
Beth Johnson <bethjohnson3@gmail.com>
2018-04-07 18:27:31 -0700

    test(client/src/components): refactor tests to mock child components and add new expected props

--- client/src/components/__tests__/App.test.jsx
+++ client/src/components/__tests__/App.test.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import App from '../App';
 
+jest.mock('../Board', () => 'Board');
+
 describe('App Component', () => {
   it('renders correctly', () => {
     const domTree = renderer.create(<App />).toJSON();
--- client/src/components/__tests__/Square.test.jsx
+++ client/src/components/__tests__/Square.test.jsx
@@ -1,19 +1,38 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import Square from '../Square';
-import { Square as SquareConstructor } from '../../lib/Square';
 
 describe('Square Component', () => {
   it('renders correctly', () => {
-    const square = new SquareConstructor(0, 0);
-    const domTree = renderer.create(<Square square={square} />).toJSON();
+    const square = {
+      walls: {},
+      connector: null,
+      glyph: null,
+    };
+    const domTree = renderer.create(<Square
+      square={square}
+      row={0}
+      col={0}
+      onClick={() => {}}
+    />).toJSON();
     expect(domTree).toMatchSnapshot();
   });
 
   it('renders correctly when walls are present', () => {
-    const square = new SquareConstructor(0, 0);
-    square.modifyWalls({ top: true, bottom: true });
-    const domTree = renderer.create(<Square square={square} />).toJSON();
+    const square = {
+      walls: {
+        top: true,
+        bottom: true,
+      },
+      connector: null,
+      glyph: null,
+    };
+    const domTree = renderer.create(<Square
+      square={square}
+      row={0}
+      col={0}
+      onClick={() => {}}
+    />).toJSON();
     expect(domTree).toMatchSnapshot();
   });
 });
